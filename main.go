eeb91caa4823c9e582bbdf8d7daf695b69c1c659
Unknown <joe2010xtmf@163.com>
2013-07-25 17:46:48 +0800

    Initialize project

---
+++ main.go
@@ -0,0 +1,60 @@
+// Copyright 2013 Unknown
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// Beego Web is official blog and documentation website for Beego web framework.
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beegoweb/routers"
+)
+
+const (
+	APP_VER = "0.0.1.0725 Beta"
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Set application log level.
+	if beego.AppConfig.String("runmode") == "pro" {
+		beego.SetLevel(beego.LevelInfo)
+
+		beego.Info("Beego Web", APP_VER)
+
+		// Initialize log file.
+		os.Mkdir("./log", os.ModePerm)
+		filew := beego.NewFileWriter("log/log", true)
+		err := filew.StartLogger()
+		if err != nil {
+			beego.Critical("NewFileWriter ->", err)
+		}
+	}
+}
+
+func main() {
+	beego.AppName = "Beego Web"
+	beego.Info("Beego Web", APP_VER)
+
+	// Register routers.
+	beego.Router("/", &routers.HomeRouter{})
+
+	// Register template functions.
+
+	// For all unknown pages.
+	beego.Run()
+}
---
+++ views/header.tpl
@@ -0,0 +1,33 @@
+{{define "header_en"}}
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="shortcut icon" href="/static/img/favicon.png" />
+		<meta name="author" content="Unknown" />
+		<meta name="description" content="Beego Web is official blog and documentation website for Beego web framework" />
+		<meta name="keywords" content="Go, golang, Beego, API documentation">
+
+		 <!-- Stylesheets -->
+		 <link href="//fonts.googleapis.com/css?family=Cabin:400,700,400italic" rel="stylesheet" type="text/css">
+		<link href="/static/css/bootstrap-custom.css" rel="stylesheet" />
+		<link href="/static/css/css.css" rel="stylesheet" />
+		<link href="/static/css/main.css" rel="stylesheet" />
+{{end}}
+
+{{define "header_zh"}}
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="shortcut icon" href="/static/img/favicon.png" />
+		<meta name="author" content="无闻" />
+		<meta name="description" content="Beego Web 是 Beego Web 框架的官方博客与文档站点" />
+		<meta name="keywords" content="Go, golang, Go语言, Beego, API文档">
+
+		 <!-- Stylesheets -->
+		 <link href="//fonts.googleapis.com/css?family=Cabin:400,700,400italic" rel="stylesheet" type="text/css">
+		<link href="/static/css/bootstrap-custom.css" rel="stylesheet" />
+		<link href="/static/css/css.css" rel="stylesheet" />
+		<link href="/static/css/main.css" rel="stylesheet" />
+{{end}}
