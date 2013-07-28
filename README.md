3b5ea6df8f37140f7279a54a6155946957bfef5e
Unknown <joe2010xtmf@163.com>
2013-07-28 22:44:06 +0800

    Add license

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-beegoweb
-========
+# What is this?
 
-beego's website.
+An open source project for official blog and documentation website for Beego web framework.
+
+## License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
--- main.go
+++ main.go
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-// Beego Web is official blog and documentation website for Beego web framework.
+// An open source project for official blog and documentation website for Beego web framework.
 package main
 
 import (
